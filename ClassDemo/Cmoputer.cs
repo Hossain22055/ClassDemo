f34f8c98f6a9ff5d89d0cd557860561c3d50d6e4
Mohammed Hossain <jamalhossain1629@gmail.com>
2021-02-04 14:03:45 -0500

    done

---
+++ ClassDemo/Cmoputer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassDemo
+{
+    public class Cmoputer
+    {
+        public Cmoputer()
+        {
+            
+        }
+
+        public string CompanyName { get; set; }
+        public int  Price { get; set; }
+        public string WarrantyYear { get; set; }
+        public bool Workable { get; set; }
+
+        public string GetComputer {
+            get {
+                return $" Company name {CompanyName}, Price ${Price}, Year of Warranty {WarrantyYear}, " +
+                    $"and Computer is warkable condition {Workable}";
+                         }
+                     }
+
+
+    }
+}
---
+++ ClassDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClassDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // One way to Print 
+
+            Cmoputer computer = new Cmoputer();
+            computer.CompanyName = "Apple";
+            computer.Price = 1200;
+            computer.WarrantyYear = "1 Year";
+            computer.Workable = true;
+
+         
+            
+
+
+
+
+
+    
+        }
+    }
+}
